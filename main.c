ddd3bb657b8bc1babe74a8fe967141f8420e5774
Andre <andre.machoski@outlook.com>
2021-06-21 23:41:02 -0300

    Removing A4 modification

--- main.c
+++ main.c
@@ -18,24 +18,11 @@ int main(void)
     GPIOA->OSPEEDR &= ~BIT0;
     GPIOA->PUPDR |= BIT0;
 
-    //setting GPIOA0 in output mode
-    GPIOA->MODER |= 0x10;
-    // push-pull output
-    GPIOA->OTYPER &= ~(0x4);
-    GPIOA->OSPEEDR |= 0x10;
-
-    //enabling pull-down
-    //GPIOA->PUPDR &= ~(BIT0+BIT1);
-    //GPIOA->PUPDR |= BIT1;
-
-    //GPIOA->BSRR |= BIT0;
-
     while(1)
     {
         if(val == 1000000)
         {
             GPIOA->ODR ^= BIT0;
-            GPIOA->ODR |= 0x4;
             val=0;
         }
         
